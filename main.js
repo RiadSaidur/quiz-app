47c5593ac903a42cbabe29219d5fe521b1462adf
deathcrush <reidvai1998@gmail.com>
2019-10-09 00:04:19 +0600

    added: voice instructions

--- main.js
+++ main.js
@@ -1,4 +1,30 @@
+const btn = document.querySelector('.btn');
+const restart = document.querySelector('.restart');
+const speachReco = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new speachReco();
+recognition.lang = 'en-GB';
+
 window.addEventListener('load', loadQuiz);
+restart.addEventListener('click', loadQuiz);
+
+if(speachReco){
+  btn.addEventListener('click', () => {
+    recognition.start();
+  });
+}
+else console.log('sucks');
+
+recognition.onstart = () => {
+  console.log('voice is activated');
+}
+
+recognition.onerror = event => {
+  console.log(event);
+}
+
+recognition.onend = event => {
+  console.log(event);
+}
 
 function loadQuiz(){
   const api = `https://opentdb.com/api.php?amount=10&type=boolean`;
@@ -13,13 +39,40 @@ function addQuiz(quizes){
   const answerTrue = document.querySelector('.true');
   const answerFalse = document.querySelector('.false');
   const result = document.querySelector('.quiz-point');
+  const quizNumber = document.querySelector('.quiz-nummber');
   let index = 0;
   nextQuiz(quizes[0]);
+  quizNumber.textContent = ` ${index+1}/10`;
+  recognition.onresult = event => {
+    const result = event.results[0][0].transcript;
+    if(result == 'true' || result == 'false'){
+      if(quizes[index].correct_answer == result){
+        let point = result.textContent;
+        point++;
+        result.textContent = point;
+      }
+      else if(result == 'restart'){
+        loadQuiz();
+        return;
+      }
+      index++;
+      if(index >= 10){
+        showResult(result.textContent)
+        return;
+      }
+      nextQuiz(quizes[index]);
+      console.log(quizes[index]);
+    }
+    console.log(result);
+    console.log(event);
+    return;
+  }
+
   answerTrue.addEventListener('click', () => {
     if(quizes[index].correct_answer === "True"){
       let point = result.textContent;
       point++;
-      result.textContent = point;
+      result.textContent = ` ${point}`;
     }
     index++;
     if(index >= 10){
@@ -27,12 +80,13 @@ function addQuiz(quizes){
       return;
     }
     nextQuiz(quizes[index]);
+    quizNumber.textContent = ` ${index+1}/10`;
   });
   answerFalse.addEventListener('click', () => {
     if(quizes[index].correct_answer === "False"){
       let point = result.textContent;
       point++;
-      result.textContent = point;
+      result.textContent = ` ${point}`;
     }
     index++;
     if(index >= 10){
@@ -40,12 +94,15 @@ function addQuiz(quizes){
       return;
     }
     nextQuiz(quizes[index]);
+    quizNumber.textContent = ` ${index+1}/10`;
     console.log(quizes[index]);
   });
 }
 
 function nextQuiz(quiz){
-  document.querySelector('.question').textContent = quiz.question;
+  setTimeout(() => {
+    document.querySelector('.question').innerHTML = quiz.question;
+  }, 1000);
   console.log(quiz);
 }
 
